9801fa8c9a93ef1f31adfd0067a7405ebb5e7b9b
Marcel Wiessler <marcel@gaisterhand.de>
2022-09-29 19:47:27 +0200

    add dist

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,30 +1,13 @@
 import { defineConfig } from 'vite';
-import viteCompression from 'vite-plugin-compression';
-import * as path from 'path';
 
 export default defineConfig({
 
     base: "./",
-    assetsInclude: ['*'],
     build: {
         outDir: "./dist",
-        emptyOutDir: false,
-        keepNames: true,
+        emptyOutDir: false
     },
-    plugins: [
-        viteCompression({ deleteOriginFile: true }),
-    ],
-
     esbuild: {
         keepNames: true,
-    },
-
-    resolve: {
-        alias: {
-            'nested-package': () => {
-                const absPath = path.resolve(__dirname, 'node_modules/nested-package');
-                return absPath;
-            },
-        }
     }
 });
